9b1711b84001327dfe222c3cf5b079cce3b0bc5c
synk <jairomer@protonmail.com>
2024-03-04 20:35:01 +0100

    Is able to substitute currently playing tune by the one selected on the list.

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,18 @@ import { FixedSizeList as List } from "react-window";
 
 const SONG_LIST_INDEX = "https://raw.githubusercontent.com/jairomer/strudel/master/index.json";
 
-class SongList extends Component {
-  constructor(props, context) {
-    super(props, context);
-    this.state = { 
-      songList: []
+class StrudelApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tune : "// Select a tune from the list below.",
+      songList: [],
+      minireplKey: 0,
     }
+  }
+
+  _switchMiniReplKey() {
+    this.setState({minireplKey: this.state.minireplKey === 0 ? 1 : 0});
   }
 
   _fetchSongList() {
@@ -20,24 +26,29 @@ class SongList extends Component {
       .then((data) => {
         this.setState({
           songList: data,
-        })
+        });
       })
       .catch((error) => {
-      console.log(error);
-    });
+        console.log(error);
+      });
   }
 
-  _renderLoading() {
-    console.log("Loading...");
-    return (
-      <>
-        <p className="List">Loading...</p>
-      </>
-    );
+  _onSongSelectionCb(songUrl) {
+    console.log(songUrl);
+    const songPromise = fetch(songUrl);
+    songPromise
+      .then((response) => response.text())
+      .then((data) => {
+        this.setState({ tune: data });
+        this._switchMiniReplKey();
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
-  _renderList(onSongSelectionCb) {
-    console.log("Redering list...");
+  _renderList() {
     const getItemClass = (key) => {
       return (key % 2 == 0) ? "ListItemEven" : "ListItemOdd";
     }
@@ -48,7 +59,7 @@ class SongList extends Component {
             <li 
               key={key}
               className={"Row " + getItemClass(key)}
-              onClick={()=>{onSongSelectionCb(song.path)}}
+              onClick={()=>{this._onSongSelectionCb(song.path)}}
             >
               {song.name}
             </li>
@@ -58,60 +69,26 @@ class SongList extends Component {
     );
   }
 
-  render() {
-    //console.log(this.props.onSongSelectionCb);
-    const onSongSelectionCb = this.props.onSongSelectionCb === undefined ? 
-      (url)=>{ console.log(url); } :
-      (url)=>{ this.props.onSongSelectionCb(url); }
-
-    if (this.state.songList.length === 0 ) {
-      this._fetchSongList();
-    }
-    const renderedList = this.state.songList.length === 0 ? 
-      this._renderLoading() : this._renderList(onSongSelectionCb);
-    return (
-      <>
-        { renderedList }
-      </>
-    );
-  }
-
-}
-
-class StrudelApp extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tune : "// Select a tune from the list below.",
-    }
-  }
-
-  _onSongSelectionCb = songUrl => {
-    console.log(songUrl);
-    const songPromise = fetch(songUrl);
-    songPromise
-      .then((response) => response.text())
-      .then((data) => {
-        console.log(data);
-        this.setState({ tune: data });
-      })
-      .catch((error) => {
-      console.log(error);
-    });
-  }
+  _renderLoading() { return (<><p className="List">Loading...</p></>); }
 
   render() {
     /*TODO:
-      - Substitute currently playing tune by the one selected on the list.
       - Add search bar for the index and dinamically update recomendations.
     */
+    console.log("Render");
+    if (this.state.songList.length === 0 ) {
+      this._fetchSongList();
+    }
+
+    const renderedList = this.state.songList.length === 0 ? 
+      this._renderLoading() : this._renderList();
     return (
       <>
         <div className="m-10 bg-neutral-800 justify-center items-center ">
-          <MiniRepl className="minirepl" tune={this.state.tune} />
+          <MiniRepl key={this.state.minireplKey} className="minirepl" tune={this.state.tune} />
         </div>
         <div className="m-10 bg-neutral-800 justify-center items-center ">
-          <SongList onSongSelectionCb={ this._onSongSelectionCb }/>
+          { renderedList } 
         </div>
       </>
     )
